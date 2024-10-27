4305e93ceccd791554def722eca475e192fccdc7
pradip dighe <dighep155@gmail.com>
2024-10-27 07:07:11 +0000

    learn var

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 144553
+let accountEmail="dighe155@gmail.com"
+var accountPassword="126516"
+let accountCity="jaipur"
+let accountState;
+/*
+prefaer not use var
+becuse issue in block scope and functional scope
+*/
+// accountId = 2 // not allowed
+
+accountEmail="hdhd@.com"
+accountPassword="254125"
+accountCity="pune"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
