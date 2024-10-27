c066737876d1a3a8ef8ac8f611e2b4055404d9fc
pradip dighe <dighep155@gmail.com>
2024-10-27 14:57:49 +0000

    data types

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all code is newer version
+
+//alert(3+3) // we are using node js,not browser
+
+console.log(3
+    +3);//code redability
+ console.log("pradip");
+ 
+ let name ="pradip"
+ let age=19
+ let loggedIn = false
+
+//primitive data type
+ //number => small or big number this range 53 something
+ //bigint=> more  bigger number
+ //string => double code using
+ //boolean => true/false
+ //null=> standlone value
+ //undefined 
+ //symbol => find unique
+
+
+ //object
+
+ console.log(typeof undefined);//undefined
+ console.log(typeof null);//object
+ 
